3fbc3065029d9825cbf315b09acef29c399256fd
Abe Isleem <abisleem@microsoft.com>
2021-10-13 17:41:20 -0700

    connects front end to blockchain

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import './App.css';
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import './App.css'
+import { ELECTION_ABI, ELECTION_ADDRESS } from './config'
+// import TodoList from './TodoList'
 
-function App() {
-  return (
-    <div className="container">
-      Hello World
-    </div>
-  );
+class App extends Component {
+
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    console.log(Web3.givenProvider);
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const electionContract = new web3.eth.Contract(ELECTION_ABI, ELECTION_ADDRESS);
+    this.setState({ electionContract });
+    console.log("electionContract", electionContract);
+    const openTime = Math.floor(Date.now() / 1000);
+    const closeTime = openTime + 180;
+
+    const candidateCount = await electionContract.methods.candidatesCount().call();
+    console.log(candidateCount);
+
+    // this.setState({ taskCount })
+    // for (var i = 1; i <= taskCount; i++) {
+    //   const task = await todoList.methods.tasks(i).call()
+    //   this.setState({
+    //     tasks: [...this.state.tasks, task]
+    //   })
+    // }
+    // this.setState({ loading: false })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: ''
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Hello World </h1>
+        <p>Account: {this.state.account}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,143 @@
+export const ELECTION_ADDRESS = '0x2E0f67feF8a98Bb2A20A0F06f051f4e50cf9f7F0'
+
+export const ELECTION_ABI = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "uint256",
+                "name": "_candidateId",
+                "type": "uint256"
+            }
+        ],
+        "name": "votedEvent",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "candidates",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "id",
+                "type": "uint256"
+            },
+            {
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "voteCount",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "candidatesCount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "voters",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_openTime",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_closeTime",
+                "type": "uint256"
+            }
+        ],
+        "name": "setOpenAndCloseTimes",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_name",
+                "type": "string"
+            }
+        ],
+        "name": "addOfficialCandidate",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_candidateId",
+                "type": "uint256"
+            }
+        ],
+        "name": "voteForOfficialCandidate",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_name",
+                "type": "string"
+            }
+        ],
+        "name": "voteForCandidateName",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
